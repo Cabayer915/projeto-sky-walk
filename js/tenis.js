c9a22598126018cbb676522ac7b599fe73b3907f
Cabayer915 <caua.bonfim@sptech.school>
2023-12-14 14:24:39 -0300

    Adicionando os primeiros Tenis do Site

--- js/tenis.js
+++ js/tenis.js
@@ -1,10 +1,110 @@
-let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:25.00, sizes:['100g', '530g', '860g'], description:'Molho de tomate, camada dupla de mussarela e orégano'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:26.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:28.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'},
-    {id:4, name:'Brasileira', img:'images/pizza4.png', price:30.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'},
-    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:29.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:35.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'},
-    {id:7, name:'Banana com canela', img:'images/pizza7.png', price:32.00, sizes:['320g', '530g', '860g'], description:'Mussarela, banana, canela e açúcar'},
-    {id:8, name:'Chocolate com morango', img:'images/pizza8.png', price:35.00, sizes:['320g', '530g', '860g'], description:'Creme de leite, lascas de chocolate e morangos'}
-];
+let pizzaJson=[
+    {
+      id: 1,
+      name: 'Air Jordan 4 Black Cat',
+      img: './assets/jordan_black_cat.jpeg',
+      price: 2999.99,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'Um clássico atemporal, o Air Jordan 4 Black Cat combina elegância e desempenho. Seu design todo preto em couro premium oferece sofisticação, enquanto a tecnologia de amortecimento na sola proporciona conforto duradouro. Este modelo icônico é uma escolha marcante para quem busca estilo e qualidade incomparáveis.'
+    },
+    {
+      id: 2,
+      name: 'VANS OLD SKOOL X PRETTY GUARDIAN SAILOR MOON',
+      img: './assets/vans_sailor_moon.jfif',
+      price: 549.99,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'A colaboração entre Vans Old Skool e Pretty Guardian Sailor Moon oferece uma fusão única de estilo urbano e nostalgia anime. Este modelo destaca-se com detalhes inspirados na Sailor Moon, mantendo a durabilidade clássica e o conforto característicos da linha Old Skool, proporcionando uma experiência única para os amantes de moda streetwear e Sailor Moon.'
+    },
+    {
+      id: 3,
+      name: 'Air Jordan 4 Fire Red',
+      img: './assets/jordan_4_retro.webp',
+      price: 1699.99,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'O Air Jordan 4 Fire Red é uma expressão de estilo e energia. Com sua parte superior branca acentuada por toques vibrantes de vermelho, este modelo incorpora a essência arrojada da linha Jordan. Além do design icônico, a tecnologia de amortecimento oferece conforto notável. Este tênis é uma declaração de moda para os entusiastas que buscam uma fusão de elegância e atitude ousada.'
+    },
+    {
+      id: 4,
+      name: 'Air Force 1 Tiffany',
+      img: './assets/air_force_1_tiffany.jfif',
+      price: 679.00,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'Com uma paleta de cores inspirada no icônico Tiffany & Co., o design apresenta detalhes em azul turquesa e branco, conferindo uma estética luxuosa. Além do apelo visual, a durabilidade e o conforto lendários da linha Air Force 1 garantem uma experiência de uso excepcional. Este modelo é a escolha perfeita para quem busca uma fusão de estilo e sofisticação urbana.'
+    },
+    {
+      id: 5,
+      name: 'Nike Dunk Travis Scott',
+      img: './assets/nike_dunk_travis.jpg',
+      price: 699.99,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'Os Nike Dunk Travis Scott são uma colaboração ousada, combinando o estilo distintivo de Travis Scott com o design clássico dos Dunks. Com sobreposições marcantes e o logo Cactus Jack, este tênis é uma afirmação autêntica de moda para os entusiastas urbanos. Além do apelo visual, a qualidade de construção e o conforto característicos da linha Dunk garantem uma experiência de uso premium.'
+    },
+    {
+      id: 6,
+      name: 'All Star X COMME Des Garçons',
+      img: './assets/allstar_X_comme.jfif',
+      price: 899.90,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'A colaboração entre All Star e COMME des GARÇONS resulta em um tênis que personifica a fusão entre moda e streetwear. Com design minimalista e a distintiva aplicação de logotipo em coração, este modelo destaca-se pela sua simplicidade ousada. Perfeito para quem busca uma declaração de estilo que transcende o convencional, mantendo a atemporalidade do clássico All Star.'
+    },
+    {
+      id: 7,
+      name: 'Air Jordan 4 Red Thunder',
+      img: './assets/jordan_4_retro_red_thunder.webp',
+      price: 1499.99,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'Os Air Jordan 4 Red Thunder são uma explosão de energia e estilo. Com uma combinação arrojada de vermelho e preto, este modelo é uma afirmação de presença. Além do design marcante, a tecnologia de amortecimento oferece conforto excepcional. Este tênis é uma escolha audaciosa para os amantes de calçados que buscam impacto visual e desempenho.'
+    },
+    {
+      id: 8,
+      name: 'VANS KNU SKOOL',
+      img: './assets/vans_knu.jpg',
+      price: 699.99,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'Os Vans Knu Skool capturam a essência do estilo urbano com um toque moderno. Com um design arrojado que combina elementos clássicos e contemporâneos, este modelo oferece versatilidade e conforto. A fusão de materiais premium e a sola característica da Vans garantem durabilidade e um visual único. Seja para as ruas ou para um look descontraído, os Vans Knü Skool são uma escolha inconfundível para os amantes de moda urbana.'
+    },
+    {
+      id: 9,
+      name: 'PUMA 180 Felt Purple',
+      img: './assets/puma_180_felt_purple.webp',
+      price: 749.90,
+      sizes: [
+        '39',
+        '40',
+        '41'
+      ],
+      description: 'PUMA 180 Felt Purple são um convite à elegância e conforto. Com um toque de sofisticação em roxo, este modelo oferece estilo e versatilidade. A parte superior em feltro adiciona uma textura única, enquanto a tecnologia de amortecimento PUMA proporciona suporte para o dia a dia. Estes tênis são uma escolha distintiva para quem busca um equilíbrio entre moda e funcionalidade.'
+    }
+  ];
